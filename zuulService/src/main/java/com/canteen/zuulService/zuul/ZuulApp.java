47e37fb7e52a70b9c1b4ad87bc2619059958acac
ylw <759646095@qq.com>
2019-08-24 10:14:07 +0800

    添加了日志

--- zuulService/src/main/java/com/canteen/zuulService/zuul/ZuulApp.java
+++ zuulService/src/main/java/com/canteen/zuulService/zuul/ZuulApp.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @SpringBootApplication
 @EnableZuulProxy
 @Controller
